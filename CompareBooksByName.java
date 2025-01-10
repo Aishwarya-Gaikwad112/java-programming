dfb80d317cfe47181c88d9cc7c214e0126bec698
Aishwarya Gaikwad <gaikwad2309@gmail.com>
2025-01-10 21:44:49 +0530

    Add files via upload

---
+++ CompareBooksByName.java
@@ -0,0 +1,18 @@
+package com.seed.library.utility;
+
+
+import java.util.Comparator;
+
+import com.seed.library.Book;
+
+public class CompareBooksByName  implements Comparator<Book>{
+	
+	
+	
+	public int compare(Book b1, Book b2) {
+		// TODO Auto-generated method stub
+		return b1.getName().compareTo(b2.getName());
+	}
+
+}
+
